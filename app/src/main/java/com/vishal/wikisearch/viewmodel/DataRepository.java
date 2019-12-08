51981114fe5a018e4991151bea0daecd011c5a48
Vishal Vanpariya <vanpariyavishal02@gmail.com>
2019-12-08 20:38:10 +0530

    data model setup

--- app/src/main/java/com/vishal/wikisearch/viewmodel/DataRepository.java
+++ app/src/main/java/com/vishal/wikisearch/viewmodel/DataRepository.java
@@ -1,10 +1,22 @@
 package com.vishal.wikisearch.viewmodel;
 
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.vishal.wikisearch.apis.APICall;
 import com.vishal.wikisearch.apis.APIService;
+import com.vishal.wikisearch.database.DBTable;
+import com.vishal.wikisearch.database.PageDataBase;
+import com.vishal.wikisearch.database.TermsTable;
+import com.vishal.wikisearch.database.ThumbnailTable;
 import com.vishal.wikisearch.models.MainDataClass;
+import com.vishal.wikisearch.models.Page;
+import com.vishal.wikisearch.models.Terms;
+import com.vishal.wikisearch.models.Thumbnail;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,10 +34,36 @@ public class DataRepository {
     }
 
     private APICall apiCall;
+    private DBTable dbTable;
+    private ThumbnailTable thumbnailTable;
+    private TermsTable termsTable;
 
     private DataRepository(){
         apiCall= APIService.getRetrofit().create(APICall.class);
     }
+
+    LiveData<List<Page>> getfromlocal(Context context){
+        LiveData<List<Page>> data = new MutableLiveData<>();
+        PageDataBase db=PageDataBase.getDataBase(context);
+        dbTable=db.gettable();
+        data=dbTable.getdata();
+        return data;
+    }
+
+    LiveData<Thumbnail> getThumbnail(int id,Context context){
+        PageDataBase db=PageDataBase.getDataBase(context);
+        thumbnailTable=db.getThumbnailTable();
+        return thumbnailTable.getThumbnail(id);
+    }
+
+    LiveData<Terms> getTerms(int id,Context context){
+        PageDataBase db=PageDataBase.getDataBase(context);
+        termsTable=db.gettermtable();
+        return termsTable.getterms(id);
+    }
+
+
+
 
     MutableLiveData<MainDataClass> getResults(String searchvalue){
         final MutableLiveData<MainDataClass> data = new MutableLiveData<>();
@@ -36,11 +74,11 @@ public class DataRepository {
                 1,
                 2,
                 "thumbnail",
-                50,
-                10,
+                200,
+                30,
                 "description",
                 searchvalue,
-                20).enqueue(new Callback<MainDataClass>() {
+                30).enqueue(new Callback<MainDataClass>() {
             @Override
             public void onResponse(Call<MainDataClass> call, Response<MainDataClass> response) {
                 if (response.isSuccessful())
--- app/src/main/java/com/vishal/wikisearch/viewmodel/ResultModelClass.java
+++ app/src/main/java/com/vishal/wikisearch/viewmodel/ResultModelClass.java
@@ -1,6 +1,7 @@
 package com.vishal.wikisearch.viewmodel;
 
 import android.app.Application;
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -8,6 +9,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.vishal.wikisearch.models.MainDataClass;
+import com.vishal.wikisearch.models.Page;
+import com.vishal.wikisearch.models.Terms;
+import com.vishal.wikisearch.models.Thumbnail;
+
+import java.util.List;
 
 public class ResultModelClass extends AndroidViewModel {
 
@@ -22,4 +28,17 @@ public class ResultModelClass extends AndroidViewModel {
     public LiveData<MainDataClass> getrepo(String serchvalue){
         return repository.getResults(serchvalue);
     }
+
+    public LiveData<List<Page>> getfromlocal(Context context){
+        return repository.getfromlocal(context);
+    }
+
+    public LiveData<Thumbnail> getThumbnail(Context context,int id){
+        return repository.getThumbnail(id,context);
+    }
+
+    public LiveData<Terms> getTerms(Context context,int id){
+        return repository.getTerms(id,context);
+    }
+
 }
